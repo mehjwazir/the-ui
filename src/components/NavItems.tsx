b23b63e79a017bc3ee1103becdd2bffc1474fa2a
mehjwazir <mehjwazir@gmail.com>
2024-01-26 01:31:30 -0800

    added icons category in product categories in index.ts. In NavItems.tsx passed the product categories and NavItem passing props from NavItem.tsx.Created NavItem.tsx with type Category and added typeof passing the product categories added NavItemProps and destructured it in the NavItem component function by adding isAnyOpen, category, handleOpen, and isOpen.

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,5 +1,7 @@
 "use client"
+import { PRODUCT_CATEGORIES } from "@/config";
 import { useState } from "react";
+import NavItem from "./NavItem";
 
 
 
@@ -7,8 +9,32 @@ const NavItems = () => {
 
 	const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+	const isAnyOpen = activeIndex !== null
+
 	return (
-		<div className="flex gap-4 h-full"></div>
+		<div className="flex gap-4 h-full">
+			{PRODUCT_CATEGORIES.map((category, i) => {
+
+				const handleOpen = () => {
+					if (activeIndex == i) {
+						setActiveIndex(null)
+					} else {
+						setActiveIndex(i)
+					}
+				}
+				const isOpen = i === activeIndex
+				return (
+					<NavItem
+						category={category}
+						handleOpen={handleOpen}
+						isOpen={isOpen}
+						key={category.value}
+						isAnyOpen={isAnyOpen}
+					/>
+				)
+			})}
+
+		</div>
 	)
 }
 
--- src/config/index.ts
+++ src/config/index.ts
@@ -17,8 +17,30 @@ export const PRODUCT_CATEGORIES = [
 				name: "New Arrivals",
 				href: "#",
 				imageSrc: "/nav/ui-kits/purple.jpg"
+			},
+		],
+	},
+	{
+		label: "Icons",
+		value: "icons" as const,
+		featured: [
+			{
+				name: "Favorite Icon Picks",
+				href: "#",
+				imageSrc: "/nav/icons/picks.jpg"
+			},
+			{
+				name: "New Arrivals",
+				href: "#",
+				imageSrc: "/nav/icons/new.jpg"
+			},
+			{
+				name: "Bestselling Icons",
+				href: "#",
+				imageSrc: "/nav/icons/bestsellers.jpg"
 			}
 		]
 	}
+
 	
 ]
