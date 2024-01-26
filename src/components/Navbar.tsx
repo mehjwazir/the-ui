24104aeee6e9dacee2c96943738130c0c7321e28
mehjwazir <mehjwazir@gmail.com>
2024-01-25 22:05:11 -0800

    created NavItems.tsx file in components with setter funtion and importing state passed NavItems in Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Icons } from "./Icons";
+import NavItems from "./NavItems";
 
 const Navbar = () => {
 	return (
@@ -12,6 +13,9 @@ const Navbar = () => {
 								<a href="/"> 
 									<Icons.logo className="h-12 w-12" />
 								</a>
+							</div>
+							<div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+								<NavItems />
 							</div>
 						</div>
 					</div>
