f3c7ad0926b81a497cf7672d910ae923280a386c
mehjwazir <mehjwazir@gmail.com>
2024-01-25 21:00:28 -0800

    added const perks array with icons from lucide installed lucide package imported icons. Passed perk.Icon after adding divs and classes so the icons and passed perk.description in page.tsx. Added Navbar component in components folder added code with and passed/imported MaxWidthWrapper as well as Icons from Icons.tsx which has the svg logo for the app. Created Icons.tsx fild in components and added code with svg imported react from react. In layout.tsx passed Navbar so it would should logo. Also added svg img in public folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Button, buttonVariants } from "@/components/ui/button";
+import { ArrowDownToLineIcon, Palette, SwatchBook } from "lucide-react";
 import Link from 'next/link'
+
+
+const perks = [
+  {
+    name: "Instant Download",
+    Icon: ArrowDownToLineIcon,
+    description: "Get your assets delivered to your email in seconds"
+
+  },
+  {
+    name: "The User Interface",
+    Icon: SwatchBook,
+    description: "Browse UI kits"
+
+  },
+  {
+    name: "The Brand Identity",
+    Icon: Palette,
+    description: "Featuring Identities for you to choose from "
+
+  }
+]
+
 
 export default function Home() {
   return (
+    <>
     <MaxWidthWrapper>
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
 
@@ -21,8 +46,32 @@ export default function Home() {
           </Button>
         </div>
       </div>
+      </MaxWidthWrapper>
+      <section className="border-t border-gray-200 bg-gray-50">
+        <MaxWidthWrapper className="py-20">
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+            {perks.map((perk) => (
+              <div
+                key={perk.name}
+                className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
+                <div className="md:flex-shrink-0 flex justify-center">
+                  <div className="h-14 w-14 flex items-center justify-center rounded-full bg-gray-300 text-gray-500 ">
+                    {<perk.Icon  className="w-1/3 h-1/3"/>}
+                </div>
+                </div>
 
-    </MaxWidthWrapper>
+                <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                  <h3 className="text-base font-medium text-gray-900">{perk.name}</h3>
+                  <p className="me-3 text-sm text-muted-foreground">
+                    {perk.description}
+                  </p>
+                </div>
+            </div>
+          ))}
+          </div>
+      </MaxWidthWrapper>
+      </section>
+    </>
 
   ) 
 }
