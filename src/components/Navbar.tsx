014f2eff9b33279bd23a89a791c698bcb2932fb4
mehjwazir <mehjwazir@gmail.com>
2024-01-26 21:47:10 -0800

    added cart.tsx in components with code and imports see for details. Added user conditionals in navbar.tsx with buttonVariants and imports also added cart in navbar. In utils.ts added function to format price in that is connected with sheet.tsx. It slides out when cart is pressed. Added sheet and separator package from shadcn both were added to package.json and lock. added cart-empty and cart-heart images which I created in Canva added those to public folder and used in Cart.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,19 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
 import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
+
 
 const Navbar = () => {
+	const user = null
 	return (
 		<div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
 			<header className="relative bg-white">
 				<MaxWidthWrapper>
 					<div className="border-b border-gray-200">
 						<div className="flex h-16 items-center">
-							<div className="ml-4 flex lg:ml-0"> 
+							<div className="ml-4 flex lg:ml-0">
 								<Link href='/'>
 									<Icons.logo className='h-10 w-10' />
 								</Link>
@@ -18,9 +22,59 @@ const Navbar = () => {
 							<div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
 								<NavItems />
 							</div>
+							<div className="ml-auto flex items-center">
+								<div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+									{user ? null : (
+										<Link
+											href='/Sign-in'
+											className={buttonVariants({
+												variant: 'ghost'
+											})}>
+											Sign in
+										</Link>
+									)}
+									{user ? null : (
+										<span className="h-6 w-px bg-gray-200 "
+											aria-hidden="true"
+										/>
+									)}
+									{user ? (
+										<p></p>
+									) : (
+										<Link href="/sign-up"
+											className={buttonVariants({
+												variant: 'ghost',
+											})}>
+											Create account
+										</Link>
+									)}
+
+									{user ? (
+										<span
+										className="h-6 w-px bg-gray-200 "
+										aria-hidden="true"
+										/> 
+									) : null}
+									
+
+									{user ? null : (
+										<div className='flex lg:ml-6'>
+											<span
+												className='h-6 w-px bg-gray-200'
+												aria-hidden='true'
+											/>
+										</div>
+									)}
+
+									<div className='ml-4 flow-root lg:ml-6'>
+										<Cart />
+									</div>
+
+								</div>
+							</div>
 						</div>
 					</div>
-			</MaxWidthWrapper>
+				</MaxWidthWrapper>
 			</header>
 		</div>
 	)
