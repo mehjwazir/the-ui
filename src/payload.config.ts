456d9a5d146458fa60ea2eacc84f6853b6ab2ab1
mehjwazir <mehjwazir@gmail.com>
2024-01-27 01:49:07 -0800

    added code in server.ts with nexapp and nextHandler payload.logger.info. Created payload.config.ts added code and imported webpack, mongooseAdapter slateEditor path and buildconfig. added export default buildConfig object with serverUrl collections and routes. Set up admin with bundler and webpackBundler and more. Added nex-utils.ts in src folder and imported next with const port and number process.env etc for production and added nextHandler added tsconfig.server.json with extends,complierOptions module outDir noEmit and more also added nodemon.json with code paths. In package.json added dev path for payload config and cross env, nodemon, payloadcms/bundler-webpack db-mongodb and richtex-slate

---
+++ src/payload.config.ts
@@ -0,0 +1,33 @@
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { slateEditor } from "@payloadcms/richtext-slate";
+import path from "path";
+import { buildConfig } from "payload/config";
+
+export default buildConfig({
+	serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
+	collections: [],
+	routes: {
+		admin: '/sell'
+	},
+	admin: {
+		bundler: webpackBundler(),
+		meta: {
+			titleSuffix: "- The-UI",
+			favicon: "/favicon.ico",
+			ogImage: "/thumnnail.jpg"
+		/*add images with those names in public folder don't forget */
+		},
+	},
+	rateLimit: {
+		max: 2000,
+	},
+	editor: slateEditor({}),
+	db: mongooseAdapter({
+		url: process.env.MONGODB_URL!,
+	}),
+	typescript: {
+		outputFile: path.resolve(__dirname, "payload-types.ts"),
+	}
+
+})
