4b09578c7c14114565324722511854ee7c9715d0
mehjwazir <mehjwazir@gmail.com>
2024-03-08 08:09:22 -0800

    Revert "added in app folder added trpc>[trpc]>route.ts here we added the endpoint logic for fetching the request handler endpoint route imported appRouter and fetchRequestHandler exported handler as GET and handler as POST trpc estentially handles api routes from server to client in TypeScript vs REST and is easy because you don't have to do schemas or code generation. Added trpc in page.tsx signup and testing on dev tools using anyApiRoute useQuery going to delete that and add authRouter but commiting to learn and save changes"
    This reverts commit 191962e9a99665edd0fe5f088a8f7ae324412243.

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -9,7 +9,6 @@ import Link from "next/link";
 import { useForm } from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod"
 import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/validators/account-credentials-validator";
-import { trpc } from "@/trpc/client";
 
 const Page = () => {
 
@@ -21,9 +20,6 @@ const Page = () => {
 	} = useForm<TAuthCredentialsValidator>({
 		resolver: zodResolver(AuthCredentialsValidator),
 	}) 
-
-	const { data } = trpc.anyApiRoute.useQuery()
-	console.log(data)
 
 	const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
 		
