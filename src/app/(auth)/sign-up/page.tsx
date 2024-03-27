4ec47a8795cc329576e5ebb38a62e66b5912ce56
mehjwazir <mehjwazir@gmail.com>
2024-03-26 22:45:55 -0700

    added const data trpc anyApiRoute.useQuery() checking console.log(data) added this to page.tsx in sign-up auth will change again

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { useForm } from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod"
 import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/validators/account-credentials-validator";
+import { trpc } from "@/trpc/client";
 
 const Page = () => {
 
@@ -21,6 +22,11 @@ const Page = () => {
 		resolver: zodResolver(AuthCredentialsValidator),
 	}) 
 
+
+	const {data} = trpc.anyApiRoute.useQuery()
+	console.log(data)
+
+	
 	const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
 		
 	}
